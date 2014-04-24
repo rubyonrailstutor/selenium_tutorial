10679d8b224058dbc5a15b2681dfccfef7d02b4d
John Davison <johncdavison@gmail.com>
2014-04-23 18:18:02 -0700

    edit scripts

--- restaurant_spec.rb
+++ restaurant_spec.rb
@@ -1,21 +1,30 @@
 require 'selenium-webdriver'
- 
+
 describe 'Login' do
- 
+
   before(:each) do
     @driver = Selenium::WebDriver.for :firefox
   end
- 
+
   after(:each) do
     @driver.quit
   end
- 
+
   it 'succeeded' do
-    @driver.get 'http://the-internet.herokuapp.com/login'
-    @driver.find_element(id: 'username').send_keys('tomsmith')
-    @driver.find_element(id: 'password').send_keys('SuperSecretPassword!')
-    @driver.find_element(id: 'login').submit
-    expect(@driver.find_element(class: 'success').displayed?).to be(true)
+    @driver.get 'http://rubyonrailstutor.com'
+    @driver.find_element(id: 'learnmore').click
+
+    # expect(@driver.find_element(By.cssSelector(".reveal-modal.medium.open"))).to be(true)
+    expect(@driver.find_element(id: 'join').displayed?).to be(true)
+    # wait = Selenium::WebDriver::Wait.new(:timeout => 2)
+    # wait.until { driver.find_element(:id => "join")}
+    # @driver.find_element(id: 'username').send_keys('tomsmith')
+    # @driver.find_element(id: 'password').send_keys('SuperSecretPassword!')
+    # @driver.find_element(id: 'login').submit
+
+
+
+
   end
- 
+
 end
